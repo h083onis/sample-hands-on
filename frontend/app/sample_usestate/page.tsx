4ef68384751681cd6584b7b2ef7140f2a44069bb
onishi <masaki00241117@gmail.com>
2024-10-14 17:16:47 +0900

    Second commit

---
+++ frontend/app/sample_usestate/page.tsx
@@ -0,0 +1,13 @@
+"use clinet";
+
+export default function Page(){
+    const showDialog:any = () => {
+        alert("アラート");
+    };
+
+    return (
+        <div>
+            <button onClick={showDialog()}>Click</button>
+        </div>
+    )
+};
